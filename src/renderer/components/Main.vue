2e127d6d63f0bf5cccad4a4d366c895d51954160
leegeunhyeok <lghlove0509@naver.com>
2018-11-10 21:43:19 +0900

    Change active animation style (wave)

--- src/renderer/components/Main.vue
+++ src/renderer/components/Main.vue
@@ -2,9 +2,19 @@
   <div id="main" class="content">
     <div class="panel">
       <button :class="started ? 'start-button start' : 'start-button'" @click="start">
-        {{ startButton }}
+        <transition name="fade" mode="out-in">
+          <div class="wrap-wave idle" v-if="!started" :key="'idle'">
+            <div class="wave w0"></div>
+            <div class="title">{{ startButton }}</div>
+          </div>
+          <div class="wrap-wave running" v-else :key="'running'">
+            <div class="wave w1"></div>
+            <div class="wave w2"></div>
+            <div class="wave w3"></div>
+            <div class="title">{{ startButton }}</div>
+          </div>
+        </transition>
       </button>
-      <span v-if="started" class=""></span>
     </div>
     <div class="panel">
       메인화면
@@ -45,41 +55,103 @@ export default {
 }
 
 .start-button {
-  outline: none;
   cursor: pointer;
-  border: 1px solid $main-color;
+  outline: none;
   background-color: #fff;
   border-radius: 50%;
+  border: none;
   width: 200px;
   height: 200px;
   margin: 20px 0;
   font-size: 1.8rem;
   font-weight: bold;
   transition: .2s;
+  padding: 0;
+  box-shadow: 0px 0px 1px $main-color;
+}
+
+.wrap-wave {
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  transition: .2s;
+  box-shadow: 0px 0px 20px $main-color;
+
+  .title {
+    pointer-events: none;
+    position: absolute;
+    color: #fff;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 
   &:hover {
-    box-shadow: 0px 0px 40px $main-color;
-    background-color: #fff;
-    color: $main-color;
-    transform: scale(1.1);
+    box-shadow: 0px 0px 40px $main-color; 
   }
 
-  &.start {
-    border: none;
-    box-shadow: 0px 0px 20px $main-color; 
-    animation: active-animation 1s alternate infinite;
-  }
-}
-
-@keyframes active-animation {
-  0% {
-    background-color: #fff;
-    color: #000;
-  }
-
-  100% {
+  &.idle {
     background-color: $main-color;
-    color: #fff;
+  }
+
+  &.running {
+    background-color: #fff;
+  }
+
+  .w0 {
+    top: 0;
+    width: 200px;
+    height: 200px;
+  }
+
+  .wave {
+    pointer-events: none;
+    border-radius: 43%;
+    position: absolute;
+    width: 500px;
+    height: 500px;
+    bottom: -370px;
+    left: -150px;
+  }
+
+  .w1 {
+    animation: drift 4s infinite linear;
+    background-color: $main-color;
+  }
+
+  .w2 {
+    animation: drift 5.5s infinite linear;
+    opacity: .3;
+    background-color: lighten($main-color, 10%);
+  }
+
+  .w3 {
+    animation: drift 7s infinite linear;
+    opacity: .3;
+    background-color: lighten($main-color, 10%);
   }
 }
+
+@keyframes drift {
+
+  from {
+    transform: rotate(0deg); 
+  }
+
+  from {
+    transform: rotate(360deg);
+  }
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
 </style>
